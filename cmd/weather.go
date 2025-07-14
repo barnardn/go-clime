cd4db670d6eb1fed03c0ea4eea7cfc1a0a1c9656
Norm Barnard <norm@normbarnard.com>
2025-07-13 20:07:19 -0400

    added here command

--- cmd/weather.go
+++ cmd/weather.go
@@ -18,8 +18,14 @@ import (
 // weatherCmd represents the weather command
 var weatherCmd = &cobra.Command{
 	Use:   "weather",
-	Short: "Returns the current weather conditions",
+	Short: "Returns the current weather conditions at the provide zip code",
 	Run:   runWeather,
+}
+
+var weatherHereCmd = &cobra.Command{
+	Use:   "here",
+	Short: "Returns the weather conditions at the current location",
+	Run:   runWeatherHere,
 }
 
 var (
@@ -31,24 +37,19 @@ var (
 
 func init() {
 	rootCmd.AddCommand(weatherCmd)
+	rootCmd.AddCommand(weatherHereCmd)
 	weatherCmd.Flags().BoolVarP(&isImperial, "imperial", "i", false, "Display imperial measurements")
 	weatherCmd.Flags().BoolVarP(&isQuiet, "quiet", "q", false, "Quiet mode. Don't show progress indicator")
 	weatherCmd.Flags().BoolVarP(&justTemp, "just-temp", "t", false, "Return only the temperature")
 	weatherCmd.Flags().BoolVarP(&justFeelsLike, "just-feelslike", "f", false, "Return only the feels like temperature")
+
+	weatherHereCmd.Flags().BoolVarP(&isImperial, "imperial", "i", false, "Display imperial measurements")
+	weatherHereCmd.Flags().BoolVarP(&isQuiet, "quiet", "q", false, "Quiet mode. Don't show progress indicator")
+	weatherHereCmd.Flags().BoolVarP(&justTemp, "just-temp", "t", false, "Return only the temperature")
+	weatherHereCmd.Flags().BoolVarP(&justFeelsLike, "just-feelslike", "f", false, "Return only the feels like temperature")
 }
 
-func runWeather(cmd *cobra.Command, args []string) {
-	if len(args) == 0 {
-		cmd.Help()
-		os.Exit(1)
-	}
-	units := clime.Metric
-	if isImperial {
-		units = clime.Imperial
-	}
-	climeClient := clime.NewClient(
-		openweathermap.NewClient(viper.GetString("apikey"), isImperial),
-	)
+func runWeatherHere(cmd *cobra.Command, args []string) {
 	whirlyType := whirly.Kitt
 	if isQuiet {
 		whirlyType = whirly.Empty
@@ -56,7 +57,26 @@ func runWeather(cmd *cobra.Command, args []string) {
 	progress := whirly.New(whirlyType)
 	progress.Start()
 
-	ccChan, errChan := climeClient.AsyncConditions(args[0])
+	ipAddress, err := runIPfetch()
+	if err != nil {
+		progress.Stop()
+		log.Fatalf("%+v\n", err)
+	}
+	location, err := geocode(*ipAddress)
+	if err != nil {
+		progress.Stop()
+		log.Fatalf("%+v\n", err)
+	}
+
+	units := clime.Metric
+	if isImperial {
+		units = clime.Imperial
+	}
+	climeClient := clime.NewClient(
+		openweathermap.NewClient(viper.GetString("apikey"), isImperial),
+	)
+
+	ccChan, errChan := climeClient.AsyncConditions(location.ZipCode)
 	select {
 	case err := <-errChan:
 		progress.Stop()
@@ -71,5 +91,52 @@ func runWeather(cmd *cobra.Command, args []string) {
 		} else {
 			fmt.Print(cc.String())
 		}
+	}
+
+}
+
+func runWeather(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		cmd.Help()
+		os.Exit(1)
+	}
+	whirlyType := whirly.Kitt
+	if isQuiet {
+		whirlyType = whirly.Empty
+	}
+	progress := whirly.New(whirlyType)
+
+	progress.Start()
+	cc, err := currentConditions(args[0])
+	progress.Stop()
+
+	if err != nil {
+		log.Fatalf("%+v\n", err)
+	}
+
+	if justTemp {
+		fmt.Println(cc.TemperatureDetails.Current.String())
+	} else if justFeelsLike {
+		fmt.Println(cc.TemperatureDetails.FeelsLike.String())
+	} else {
+		fmt.Print(cc.String())
+	}
+}
+
+func currentConditions(zipCode string) (*clime.CurrentConditions, error) {
+	units := clime.Metric
+	if isImperial {
+		units = clime.Imperial
+	}
+	climeClient := clime.NewClient(
+		openweathermap.NewClient(viper.GetString("apikey"), isImperial),
+	)
+	ccChan, errChan := climeClient.AsyncConditions(zipCode)
+	select {
+	case err := <-errChan:
+		return nil, err
+	case conditions := <-ccChan:
+		cc := clime.NewCurrentConditions(conditions, units)
+		return &cc, nil
 	}
 }
